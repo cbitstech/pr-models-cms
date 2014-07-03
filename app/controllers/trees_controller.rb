383862456649e605a8dc1f7cc3a65e1d1e22f4f3
Gwen Smuda <gwen.smuda@gmail.com>
2014-07-03 10:28:04 -0500

    working version so far

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,19 +1,17 @@
 class TreesController < ApplicationController
 
 	def new
+  	@tree = Tree.new
 	end
 
 	def create
 		@tree = Tree.new(tree_params)
 
-		respond_to do |format|
-				  if @tree.save
-				    format.html { redirect_to @tree, notice: 'Your metadata was saved.' }
-				    format.json { render action: 'show', status: :created, location: @tree }
-				  else
-				    format.html { render action: 'new' }
-				    format.json { render json: @treer.errors, status: :unprocessable_entity }
-			end
+		if @tree.save
+		  redirect_to @tree
+		else
+		  render 'new'
+		end
 	end
 
 	def index
@@ -24,25 +22,23 @@ class TreesController < ApplicationController
 		@tree = Tree.find(params[:id])
 	end
 
+	def update
+  	@tree = Tree.find(params[:id])
+ 
+		if @tree.update(tree_params)
+		  redirect_to @tree
+		else
+		  render 'edit'
+		end
+	end
+
 	def show
 		@tree = Tree.find(params[:id])
 	end
 
-	def update
-    respond_to do |format|
-      if @tree.update(tree_params)
-        format.html { redirect_to @tree, notice: 'Tree metadata successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @tree.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-private
+	private
 	def tree_params
-  	params.require(:tree).permit(:author, :accuracy)
+		params.require(:tree).permit(:author, :accuracy)
 	end
-
 end
+
