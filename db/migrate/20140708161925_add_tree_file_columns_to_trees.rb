bd9c3b02d43ae6ac2ba16e17335ab6fd820bf764
Gwen Smuda <gwen.smuda@gmail.com>
2014-07-23 13:17:00 -0500

    removing depricated migrations

--- db/migrate/20140708161925_add_tree_file_columns_to_trees.rb
+++
@@ -1,9 +0,0 @@
-class AddTreeFileColumnsToTrees < ActiveRecord::Migration
-  def self.up
-    add_attachment :trees, :tree_file
-  end
-
-  def self.down
-    remove_attachment :trees, :tree_file
-  end
-end
--- db/migrate/20140710144721_drop_treefiles.rb
+++
@@ -1,5 +0,0 @@
-class DropTreefiles < ActiveRecord::Migration
-  def change
-		 drop_table :tree_files
-  end
-end
